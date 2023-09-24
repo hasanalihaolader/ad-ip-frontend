096728984bee739611cf6de4b4261fcdd0fac088
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-09-24 23:48:40 +0600

    feature: create api instance to call apigw api

--- src/helpers/api_instance.js
+++ src/helpers/api_instance.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 const api_instance = axios.create({
-  baseURL: process.env.APIGW_BASE_URL, // Replace with your API server URL
+  baseURL: process.env.VUE_APP_APIGW_BASE_URL, // Replace with your API server URL
 });
 
 export default api_instance;
