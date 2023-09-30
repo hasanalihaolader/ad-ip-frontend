4cff69c0121a51ddd9c494e6af789d129ac91cb9
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-10-01 01:24:43 +0600

    feature: refine text

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,10 +4,10 @@
             <nav class="navbar  navbar-expand-lg navbar-default">
                 <div class="container-fluid">
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="#"><b>AD - IP Management Solution</b></a>
+                        <router-link class="navbar-brand" to="/dashboard"><b>AD - IP Management Solution</b></router-link>
                     </div>
                     <ul class="nav navbar-nav">
-                        <li class="active"><router-link to="/dashboard/ip">IPs</router-link></li>
+                        <li><router-link to="/dashboard/ip">IPs</router-link></li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li><router-link to="#"><span @click="logout">Logout</span></router-link></li>
@@ -21,6 +21,7 @@
         </div>
     </div>
 </template>
+
 
 <script>
 import AuthRepository from '../Repositories/AuthRepository';
