fc4803b163f156cdf2b095af2ad2da398bc4d23f
bs23.hasan <hasan.haolader@brainstation-23.com>
2023-10-02 00:24:48 +0600

    feature: dockerized

--- src/Middleware/DynamicPageTitle.js
+++
@@ -1,8 +0,0 @@
-export function dynamicPageTitle(router) {
-    router.beforeEach((to, from, next) => {
-        window.document.title = to.meta.page_title !== undefined ? to.meta.page_title : 'Home';
-        next();
-    });
-}
-
-
